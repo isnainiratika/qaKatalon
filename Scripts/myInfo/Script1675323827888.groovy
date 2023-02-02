4e9c169d7e9ad9dfd56dc09eed70e7bc5a5ac662
isnaini atika <atika.pragma@gmail.com>
2023-02-02 14:53:48 +0700

    second commit

--- Scripts/myInfo/Script1675323827888.groovy
+++ Scripts/myInfo/Script1675323827888.groovy
@@ -17,17 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.callTestCase(findTestCase('OrangeLogin'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-
-WebUI.maximizeWindow()
-
-WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input_Username_username'), 'Admin')
-
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
-
-WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/button_Login'))
-
-WebUI.delay(3)
+WebUI.click(findTestObject('Page_OrangeHRM/a_My Info'))
 
